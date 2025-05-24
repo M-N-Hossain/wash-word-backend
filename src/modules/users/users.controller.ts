323f3854594b5f667297c177b7c475c7f4c40de9
OskarEriksen <omle.eriksen@gmail.com>
2025-05-24 22:22:50 +0200

    Updated how the user retrieves their data

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  // Param,
+  // ParseIntPipe,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
@@ -6,9 +13,15 @@ import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  // @UseGuards(JwtAuthGuard)
+  // @Get(':id')
+  // findOne(@Param('id', ParseIntPipe) id: string) {
+  //   return this.usersService.findOne(id);
+  // }
+
   @UseGuards(JwtAuthGuard)
-  @Get(':id')
-  findOne(id: number) {
-    return this.usersService.findOne(id);
+  @Get('me')
+  getCurrentUser(@Request() req) {
+    return this.usersService.findOne(req.user.id);
   }
 }
