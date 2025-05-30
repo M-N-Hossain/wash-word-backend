6372d0c01498bb1ca5a491ad39f5cd042e3856bd
nhnahid1122 <mdxx0356@stud.kea.dk>
2025-05-30 21:07:27 +0200

    removed some unused files

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-// import { GlobalExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Apply global pipes and filters
   app.useGlobalPipes(new ValidationPipe());
-  // app.useGlobalFilters(new GlobalExceptionFilter());
 
   // Enable CORS
   app.enableCors();
